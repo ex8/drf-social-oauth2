5478e1b7b5227ad06e9bc5a5498c09c0c131e75a
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-01 22:50:31 +0200

    feat ✨ rename model.
    Signed-off-by: Wagner de Lima <wagner_de_lima@epam.com>

--- drf_social_oauth2/models.py
+++ drf_social_oauth2/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.db.models import CharField, Model
+from django.db.models import CharField, Model, IntegerField
 
 
-class MultiFactorToken(Model):
+class MultiFactorAuth(Model):
     id = models.BigAutoField(primary_key=True)
-    token = CharField(max_length=255, unique=True, blank=False, null=False)
+    token = CharField(max_length=255, unique=True, blank=False, null=False, db_index=True)
     backend = CharField(max_length=100, unique=False, blank=False, null=False)
     client_id = CharField(max_length=100, unique=False)
     client_secret = CharField(max_length=255, unique=False)
+    code = CharField(max_length=6, blank=False, null=False, db_index=True)
 
     expires = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
