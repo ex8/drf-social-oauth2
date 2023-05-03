7b586c77c66e068a10420f1767ad6bf8fecdf7c7
Wagner de Lima <wagner_de_lima@epam.com>
2023-05-03 23:08:16 +0200

    feat: ✨ remove the unique=True from token.
    Signed-off-by: Wagner de Lima <wagner_de_lima@epam.com>

--- drf_social_oauth2/models.py
+++ drf_social_oauth2/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.db.models import CharField, Model, IntegerField
+from django.db.models import CharField, Model
 
 
 class MultiFactorAuth(Model):
     id = models.BigAutoField(primary_key=True)
-    token = CharField(max_length=255, unique=True, blank=False, null=False, db_index=True)
+    token = CharField(max_length=255, unique=False, blank=False, null=False, db_index=True)
     backend = CharField(max_length=100, unique=False, blank=False, null=False)
     client_id = CharField(max_length=100, unique=False)
     client_secret = CharField(max_length=255, unique=False)
@@ -14,7 +14,7 @@ class MultiFactorAuth(Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    # @todo - definse expiration of token
+    # @todo - define expiration of token
     def is_expired(self):
         pass
 
